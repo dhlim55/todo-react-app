b017f8fd3297b7a1f2b8a7882d44f0ebfedc4c8a
dhlim55 <dla0889@naver.com>
2023-04-24 16:00:51 +0900

    4/24 3.3절 완료

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Todo from './Todo';
 import AddTodo from './AddTodo.js';
 import { Paper,List, Container } from '@material-ui/core';
+import { call } from "./service/ApiService";
 
 class App extends React.Component{
   constructor(props) {
@@ -12,31 +13,42 @@ class App extends React.Component{
             ], 
     };
   }
-//(1)함수추가
-add = (item) => {
-  const thisItems = this.state.items;
-  item.id = "ID-" + thisItems.length;
-  item.done = false;
-  thisItems.push(item);
-  this.setState({ items: thisItems });
-  console.log("items: ", this.state.items);
+
+componentDidMount() {
+  call("/todo", "GET", null).then((response) =>
+    this.setState({items: response.data})
+  );
 }
 
+//(1)함수추가
+add = (item) => {
+  call("/todo", "POST", item).then((response) => 
+  this.setState({ items: response.data })
+  );
+};
+
 delete = (item) => {
-  const thisItems = this.state.items;
-  console.log("Before Update Items : ", this.state.items)
-  const newItems = thisItems.filter(e => e.id !== item.id);
-  this.setState({items: newItems}, () => {
-    console.log("Update Items : ", this.state.items)
-  });
-}
+  call("/todo", "DELETE", item).then((response)=>
+  this.setState({items: response.data})
+  );
+};
+
+update = (item) => {
+  call("/todo", "PUT", item).then((response) => 
+    this.setState({ items: response.data })
+  );
+};
 
   render() {
     var todoItems = this.state.items.length > 0 && (
       <Paper style={{ margin:16 }}>
         <List>
           {this.state.items.map((item, idx) => (
-            <Todo item={item} key={item.id} delete={this.delete} />
+            <Todo 
+            item={item}
+            key={item.id}
+            delete={this.delete}
+            update={this.update} />
           ))}
         </List>
         </Paper>
